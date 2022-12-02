ee2c855df41ea150d6d37f6eb4350062efab8575
Darya <Darsy_13@bk.ru>
2022-12-02 15:33:49 +0300

    Add Task solution

---
+++ Task/Program.cs
@@ -0,0 +1,33 @@
+﻿//Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+string[] array = new string [4] {"Вий", "Яма", "На дне", "Мцыри"};
+string[] find = new string [array.Length];
+
+void FindThreeDigitString(string[] array, string[] find)
+{
+    int size = 3;
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= size)
+        {
+            find[j] = array[i];
+            j++;
+        }
+
+    }
+
+}
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ShowArray(array);
+FindThreeDigitString(array,find);
+Console.WriteLine();
+ShowArray(find);
